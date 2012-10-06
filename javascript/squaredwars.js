1558135e961bf533d6453b0364b77163bfb0fbe3
romaintailhurat <romaintailhurat@MacBook-Air-de-Romain.local>
2012-10-06 22:35:56 +0200

    Timer + Resource stack and view + Sonde view

--- javascript/squaredwars.js
+++ javascript/squaredwars.js
@@ -13,15 +13,75 @@ var TYPES = ['T','R'],
 //----- Module contenant la partie en cours
 var GAME = {};
 
-var playerResources = 0;
+//duration in seconds
+GAME.duration = 0;
+
+//----- Timer
+
+YUI().use('event', function(Y) {
+  //-tick
+  setInterval(function() {Y.fire('clock:tick');},1000);
+  //-event listener
+  Y.on('clock:tick', function() {
+    GAME.duration += 1;
+    //debug('duration : ' + duration);
+    //display seconds on page
+    $('#duration').html(GAME.duration + "secs");
+  });
+});
+
 
 //----- Models
-
+//--- Grid
 var GridModel = Backbone.Model.extend({});
 
+//--- Sonde
 var SondeModel = Backbone.Model.extend({
   defaults : {
     'value' : 3 
+  }
+});
+
+//--- ResourceStack
+/*
+Holds resources held by the player
+*/
+var ResourceStackModel = Backbone.Model.extend({
+  //player starts with 3 resources
+  defaults : {
+    'stack' : 5
+  },
+
+  add : function(quant) {
+    debug('adding ' + quant + ' resources to stack');
+    var currentStack = this.get('stack');
+    this.set('stack',currentStack + quant);
+    //associated view will (must) be updated 'change' is triggered
+    this.trigger('change'); 
+    if (this.get('stack') >= 30) {
+      alert('GAME OVER - YOU WIN');
+    }
+  },
+
+  subtract : function(quant) {
+    debug('subtracting ' + quant + ' to resource stack');
+    if(this.isSubtractionPossible(quant)) {
+      var currentStack = this.get('stack');
+      this.set('stack', currentStack - quant);
+      return true;
+    }
+    else {
+      return false;
+    }
+  },
+
+  isSubtractionPossible : function(quant) {
+    if(this.get('stack') - quant >= 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 });
 
@@ -29,7 +89,7 @@ var SondeModel = Backbone.Model.extend({
 
 var GridView = Backbone.View.extend({
   render : function() {
-    debug("this.el.id : " + this.el.id);
+    debug("rendering grid view to div with id : " + this.el.id);
 
     var gridToBeDisplayed = this.model.get('grid'),
         elId = '#'+this.el.id;
@@ -40,6 +100,25 @@ var GridView = Backbone.View.extend({
 
 var SondeView = Backbone.View.extend({});
 
+var ResourceStackView = Backbone.View.extend({
+  el : '#resource-area',
+
+  //template : _.template($('#template-resource').html()),
+
+  render : function() {
+    debug('rendering resource stack view');
+
+    var context = {
+          'stack' : this.model.get('stack')
+        },
+        //ideally, would be a view property ! but this.template won't work !  =(
+        template = _.template($('#template-resource').html()),
+
+        html = template(context);
+
+        $(this.el).html(html);
+  }
+});
 //----- Logic
 
 //-------------- UTILS

d1ad8278db727a9074ba390cb5d1f3c7ed25301d
romaintailhurat <romaintailhurat@MacBook-Air-de-Romain.local>
2013-04-08 21:07:35 +0200

    Suppression playcraft sauf distribution + je m'amuse avec Entity Component System

---
+++ bhwh/scripts/systems.js
@@ -0,0 +1,9 @@
+/**
+Ensemble des systèmes utilisé par le jeu
+*/
+
+var
+
+inputSystem = pc.systems.Input.extend('inputSystem',
+ {},
+ {});
---
+++ bhwh/scripts/tweaks.js
@@ -0,0 +1,13 @@
+/*
+TWEAKS
+*/
+
+// FIXME idéalement, sera chargé à partir d'un JSON de config ?
+var TWEAKS = {
+	"US" : {
+		machinesCount : 100
+	},
+	"China" : {
+		machinesCount : 200
+	}
+};

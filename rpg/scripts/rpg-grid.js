2172b37d9822a91505f3f5c0cf150a243551de80
romaintailhurat <romaintailhurat@MacBook-Air-de-Romain.local>
2013-05-15 22:07:37 +0200

    plus de crafty

--- rpg/scripts/rpg-grid.js
+++ rpg/scripts/rpg-grid.js
@@ -2,16 +2,18 @@ var RPG = RPG || {};
 
 RPG.grid = {
 	// number of tiles on y
-	height : 32,
+	height : 16,
 	// number of tiles on x
-	width : 64,
+	width : 16,
 	tile : {
 		height : 16,
 		width : 16
 	},
+
 	getGameHeight :  function() {
 		return this.height * this.tile.height;
 	},
+
 	getGameWidth : function() {
 		return this.width * this.tile.width;
 	}

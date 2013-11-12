043abe699c756764a275fc204ec956c9ae06e839
romaintailhurat <romaintailhurat@MacBook-Air-de-Romain.local>
2013-11-12 21:35:49 +0100

    CW  : communication channel setup

--- crystalwars/cw.py
+++ crystalwars/cw.py
@@ -4,6 +4,10 @@ __author__ = 'romaintailhurat'
 import webapp2
 import jinja2
 import os
+import json
+
+from google.appengine.api import channel
+
 from logging import *
 
 import models
@@ -32,11 +36,36 @@ class CWGameHandler(webapp2.RequestHandler):
     """
 
     def get(self, gameId):
+        """
+        Display game each player
+        """
         game = models.Game.get_by_id(int(gameId))
-        self.response.out.write(game.state)
+        template = jinja_environment.get_template('cw.game.html')
+        token = channel.create_channel(gameId)
+        self.response.out.write(template.render({
+            'id' : game.key().id(), # can't use gameId, makes it buggy
+            'token' : token
+            }))
 
     def post(self):
+        """
+        Game creation
+        """
         game = models.Game()
         game.init()
         game.put()
-        self.response.out.write('game created with id %s' %str(game.key().id()))
+        message = 'game created with id %s' %str(game.key().id())
+        self.response.out.write(json.dumps({'m': message }))
+
+    def put(self, gameId):
+        """
+        Game modification
+        """
+        channel.send_message(gameId,'the game was modified')
+        self.response.out.write(json.dumps({'m' : 'PUT on a game'}))
+
+    def delete(self, gameId):
+        """
+        Delete a game
+        """
+        self.response.out.write(json.dumps({'m' : 'DELETE on a game'}))
--- crystalwars/models.py
+++ crystalwars/models.py
@@ -5,6 +5,11 @@ from google.appengine.ext import db
 class Game(db.Model):
 
     state = db.StringProperty()
+    player1Ready = False
+    player2Ready = False
 
     def init(self):
         self.state = 'created'
+
+    def toJSON():
+    	None
